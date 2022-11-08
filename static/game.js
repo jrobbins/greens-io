3832397c1888156a94f866c7495bb7e461cbec77
Jason Robbins <jrobbins@gmail.com>
2022-11-08 07:21:11 +0000

    Added interpolation

--- static/game.js
+++ static/game.js
@@ -6,6 +6,16 @@ let sz = []; // Snippets
 let nick = '';
 
 
+function interp(oldObj, newObj, fraction) {
+  const result = { ...oldObj };
+  for (let key in newObj) {
+    if (typeof(oldObj[key]) == 'number' &&
+	typeof(newObj[key]) == 'number') {
+      result[key] += Math.floor((newObj[key] - oldObj[key]) * fraction);
+    }
+  }
+  return result;
+}
 
 
 function init() {
@@ -25,11 +35,19 @@ function gameLoop() {
   if (!running) return;
   if (gioClient.playerId) {
     gioClient.getArena().then((res) => {
+      const rzOrig = {...rz};
       rz = res.resources;
       tz = res.techs;
       uz = res.upgrades;
       sz = res.snippets;
-      appEl.rz = rz;
+      appEl.rz = interp(rzOrig, rz, 0.2);
+      window.setTimeout(() => {
+	appEl.rz = interp(rzOrig, rz, 0.4); }, 180);
+      window.setTimeout(() => {
+	appEl.rz = interp(rzOrig, rz, 0.6); }, 180*2);
+      window.setTimeout(() => {
+	appEl.rz = interp(rzOrig, rz, 0.8); }, 180*3);
+      window.setTimeout(() => { appEl.rz = rz; }, 180*4);
       appEl.uz = uz;
       appEl.sz = sz;
     });
